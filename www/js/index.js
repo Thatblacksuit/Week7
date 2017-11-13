2a2e526b8158333f5533e32a05a6b47e53b195cb
Thatblacksuit <bakw1_15@uni.worc.ac.uk>
2017-11-13 15:02:36 +0000

    0.0.2
    attempting beep

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,6 @@
 function submitBtn() {
     var rndm = ['True','False'][Math.round(Math.random())]
     $("#myText").text(rndm);
-    
-    if (rndm = "True"){
-    navigator.notification.beep(1);};
+    navigator.notification.beep(2);
+
 };
