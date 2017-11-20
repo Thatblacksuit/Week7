2656b2c6a33360b3289f80bc0ff29aa8a1413725
Thatblacksuit <bakw1_15@uni.worc.ac.uk>
2017-11-20 12:28:34 +0000

    beep
    beep

--- www/js/index.js
+++ www/js/index.js
@@ -1,14 +1,20 @@
+//listen for device ready
 document.addEventListener("deviceready", onDeviceReady, false);
 
 
-function onDeviceReady(){
-    $('#submitButton').on("click", getAnswer);
-    navigator.notification.beep(1);
+//when device is ready
+function onDeviceReady() {
 
-}
+    //set up click event handling for button
+  	$('#submitButton').on("click", getAnswer);
     
-            function getAnswer() {
-        var rndm = ['True','False'][Math.round(Math.random())]
-    $("#myText").text(rndm);
-   navigator.notification.beep(1);
-            }
+}
+
+
+//get a random answers
+function getAnswer() {
+    
+            var rndm = ['True','False'][Math.round(Math.random())]
+    $("#responseText").text(rndm);
+navigator.notification.beep(1);
+    }
